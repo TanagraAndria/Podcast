2a2261ecc5acbb110f9a6ab63d8daf948ee4793c
Tanagra Andria <design.andria@gmail.com>
2021-05-05 18:36:57 -0300

    Code review

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-Em desenvolvimento
-Trilha ReactJS Rocketseat
+![alt text](public/logo.svg)
+
+
+
+
+Tecnologias Utilizadas:
+
+    Html 
+    Sass 
+    Typescript 
+    ReactJS 
+    NextJS
+    NodeJS 
+
+
+
+
+Projeto realizado na Next Level Week #5 @Rocketseat.
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,27 @@
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
+
+import { usePlayer } from '../../contexts/PlayerContext';
+
 import styles from './styles.module.scss';
 
-export default function Header() {
-    const currentDate= format(new Date(), 'EEEEEE, d MMM', {
+export function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
         locale: ptBR,
-    })
+    });
 
- return(
-     <header className={styles.headerContainer}>
-        <img src ="/logo.svg" alt="Podcast" />
-        <p> O melhor para você ouvir</p>
-        <span>{currentDate}</span>
-     </header>
- );
+    const { toggleDarkMode, isDarkModeActive} = usePlayer(); 
+
+    return (
+        <header className={isDarkModeActive ? styles.headerDarkMode : styles.headerContainer}>
+
+            <img src="/logo.svg" alt="Podcastr" />
+
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>{currentDate}</span>
+
+            <button type="button" onClick={toggleDarkMode}>Dark Mode</button>
+        </header>
+    );
 }
